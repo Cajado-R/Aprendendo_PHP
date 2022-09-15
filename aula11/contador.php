e8fe851d6b7fc398e3f61e0881e56b9d982ec20f
Cláudio Cajado <101744023+Cajado-R@users.noreply.github.com>
2022-09-15 01:37:24 -0300

    Exercicio completo.

--- aula11/contador.php
+++ aula11/contador.php
@@ -13,16 +13,70 @@
     $n2 = isset($_GET["fin"])?$_GET["fin"]:0;
     $selection = $_GET["select"];
     switch($selection){
+        
         case 2: 
-       while($n1 <= $n2){
-        echo "$n1";
-        $n1 = $n1 + 2;
-       }
+            while($n1 < $n2){
+            echo ("\n $n1");
+            $n1 += 2;
+            
+        }
+    
+        case 2: 
+        while ($n2 < $n1){
+            echo ("\n $n2");
+            $n2 +=2;
+        }
+        break;
+
+        case 4: 
+            while($n1 < $n2){
+                echo ("\n $n1");
+                $n1 += 4;
+
+        } 
+
+        case 4: 
+            while($n2 < $n1){
+                echo ("\n $n2");
+                $n2 += 4;
+        } 
+        break;
+
+        case 6: 
+            while($n1 < $n2){
+                echo ("\n $n1");
+                $n1 +=6;
+        }
+        case 6: 
+            while($n2 < $n1){
+                echo ("\n $n2");
+                $n2 +=6;
+        }
+        break;
+        case 8: 
+            while($n1 < $n2){
+                echo ("\n $n1");
+                $n1 += 8;
+        }
+        case 8: 
+            while($n2 < $n1){
+                echo ("\n $n2");
+                $n2 += 8;
+        }
+        break;
+
+        case 10: 
+            while($n1 < $n2){
+                echo ("\n $n1");
+                $n1 += 10;
+        } 
+        case 10: 
+            while($n2 < $n1){
+                echo ("\n $n2");
+                $n2 += 10;
+        }
         break;
     }
-    
-    
-
     
     ?>
 </body>
