8cbaecc78dad2010708a5486d459eab0cd2c7acf
Cláudio Cajado <101744023+Cajado-R@users.noreply.github.com>
2022-09-22 16:13:12 -0300

    Create pessoal2.php

---
+++ aula14/pessoal2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    function soma(){
+        $v = func_get_args();
+        $v2 = func_num_args();
+        $v3 = 0;
+        for ($l = 0; $l < $v2; $l++){
+
+        } return ($v3);
+    } 
+     $res soma(1, 2, 3, 4);
+     echo "a soma vale $res"
+    
+    
+    
+    
+    
+    
+    
+    ?>
+</body>
+</html>
